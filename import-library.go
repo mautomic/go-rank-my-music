4c761ea4a52c304dc2bef546d4ccb0ed13f4fb30
mautomic <maurya373@gmail.com>
2020-08-14 01:58:50 -0500

    generate list of albums using struct

--- import-library.go
+++ import-library.go
@@ -3,10 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/emirpasic/gods/sets/hashset"
 	"log"
 	"os"
 	"strings"
 )
+
+type album struct {
+	albumName  string
+	artistName string
+}
+
+func newAlbum(name string, artist string) *album {
+	a := album{albumName: name, artistName: artist}
+	return &a
+}
 
 func ImportLibrary() {
 
@@ -29,18 +40,38 @@ func ImportLibrary() {
 		log.Fatal(err)
 	}
 
-	generateAlbumsAndArtists(lines)
+	albums := generateAlbums(lines)
+	for i := 0; i < len(albums); i++ {
+		fmt.Println(albums[i])
+	}
 }
 
-func generateAlbumsAndArtists(lines []string) {
+func generateAlbums(lines []string) []album {
+
+	var albums []album
+	albumSet := hashset.New()
+	var artistNameHolder [1]string
 
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
+
+		if strings.Contains(line, "<key>Artist</key>") {
+			artistLine := strings.Split(line, "<key>Artist</key><string>")
+			artistName := strings.Split(artistLine[1], "</string>")[0]
+			artistNameHolder[0] = artistName
+			continue
+		}
+
 		if strings.Contains(line, "<key>Album</key>") {
 			albumLine := strings.Split(line, "<key>Album</key><string>")
 			albumName := strings.Split(albumLine[1], "</string>")[0]
-			fmt.Print(albumName + "\n")
+
+			if !albumSet.Contains(albumName) {
+				albumSet.Add(albumName)
+				albums = append(albums, *newAlbum(albumName, artistNameHolder[0]))
+			}
 			continue
 		}
 	}
+	return albums
 }
